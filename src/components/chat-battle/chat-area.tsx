668530c310718d758c0ac42a53935cc58841dc17
Benjamin Gutierrez <gutybv@gmail.com>
2025-02-12 13:47:30 -0300

    add: fuzz profile photo and name

--- src/components/chat-battle/chat-area.tsx
+++ src/components/chat-battle/chat-area.tsx
@@ -13,7 +13,7 @@ import { ClickableAgentAvatar } from "@/components/character/clickable-agent-ava
 const AGENT_IDS = {
     AGENT1_ID: 'e0e10e6f-ff2b-0d4c-8011-1fc1eee7cb32', // trump
     AGENT2_ID: '94bfebec-fb1a-02b3-a54d-a1f15b5668a5',  // china
-    AGENT3_ID: 'd1c10fb0-e672-079e-b9cf-a1c8cdc32b96'  // CESAR
+    AGENT3_ID: 'd1c10fb0-e672-079e-b9cf-a1c8cdc32b96'  // Fuzz
 } as const;
 
 // Modificar las constantes de los agentes para incluir más información
@@ -33,11 +33,11 @@ const AGENTS_INFO = {
         side: 'xi' as const
     },
     [AGENT_IDS.AGENT3_ID]: {
-        name: 'Cesar',
+        name: 'Fuzz',
         color: 'bg-blue-500',
-        initials: 'C',
-        avatar: '/cesar.png',
-        side: 'cesar' as const
+        initials: 'FZ',
+        avatar: '/fuzz.jpg',
+        side: 'fuzz' as const
     }
 } as const;
 
